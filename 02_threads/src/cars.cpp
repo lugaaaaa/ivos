880aef3569b5c91bd640e0cf4fdb440fe0366fc1
lugaaaaa <38163450+lugaaaaa@users.noreply.github.com>
2020-10-05 18:06:21 +0200

    klassenimplementierung mit operator

--- 02_threads/src/cars.cpp
+++ 02_threads/src/cars.cpp
@@ -14,8 +14,34 @@ void golf_mk2() {
   }
 }
 
+class Car {
+  private:
+    string carname;
+  public:
+    void operator()(){
+      int cnt{1};
+
+      while (true) {
+        cout << "Runde" << cnt << ":" << this->carname << endl;
+        this_thread::sleep_for(1s);
+        cnt++;
+      }
+    }
+    Car(string name){
+      this->carname = name;
+    }
+};
+
 int main(int argc, char const *argv[]) {
-  thread t1{golf_mk2};
+  /*thread t1{golf_mk2};
   t1.join();
+
+  car Car("Lada");
+  thread t2{Car};*/
+
+  Car mercl("C63S Coupe");
+  thread t1{ref(mercl)};
+  t1.join();
+
   return 0;
 }
