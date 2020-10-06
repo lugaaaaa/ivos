c99b4320d47067d2fd8379a62afa4da04bb73bdf
lugaaaaa <38163450+lugaaaaa@users.noreply.github.com>
2020-10-06 10:14:00 +0200

    implementierung der sieger und verlierer ausgabe und berrechnung im hauptthread

--- 02_threads/src/cars.cpp
+++ 02_threads/src/cars.cpp
@@ -10,20 +10,22 @@
 using namespace std;
 using namespace chrono;
 
-void golf_mk2() {
+void golf_mk2(double& totaltime) {
   int cnt{1};
+  double lap_time;
+
   std::random_device rd;
   std::mt19937 gen{rd()};
   std::uniform_real_distribution<> dis{1, 10};
-  double lap_time;
   ostringstream buf;
 
-  while (true) {
+  while (cnt <= 10) {
     lap_time = dis(gen);
-    buf << "Runde" << to_string(cnt) << ": golf mk2" << ": " << setprecision(3) << lap_time << "\n" << flush;
+    buf << "Runde" << to_string(cnt) << ": Golf MK2" << ": " << setprecision(3) << lap_time << "\n" << flush;
     cout << buf.str();
     buf.str("");
     this_thread::sleep_for(milliseconds(static_cast<int>(lap_time*1000)));
+    totaltime += lap_time;
     cnt++;
   }
 }
@@ -31,18 +33,23 @@ void golf_mk2() {
 class Car {
   private:
     string carname;
+    double totaltime;
   public:
     void operator()(){
       int cnt{1};
+      double lap_time;
 
       std::random_device rd;
       std::mt19937 gen{rd()};
       std::uniform_real_distribution<> dis{1, 10};
-      double lap_time;
+      ostringstream buf;
 
-      while (true) {
+      while (cnt <= 10) {
         lap_time = dis(gen);
-        cout << "Runde" << to_string(cnt) << ": " << this->carname << ": " << setprecision(3) << lap_time << "\n" << flush;
+        totaltime += lap_time;
+        buf << "Runde" << to_string(cnt) << ": " << carname << ": " << setprecision(3) << lap_time << "\n" << flush;
+        cout << buf.str();
+        buf.str("");
         this_thread::sleep_for(milliseconds(static_cast<int>(lap_time*1000)));
         cnt++;
       }
@@ -50,16 +57,33 @@ class Car {
     Car(string name){
       this->carname = name;
     }
+
+    double get_total_time(){
+      return this->totaltime;
+    }
+
+    string get_car_name(){
+      return this->carname;
+    }
 };
 
 int main(int argc, char const *argv[]) {
-  thread t1{golf_mk2};
+  double totaltime;
+  thread t1{golf_mk2, ref(totaltime)};
 
   Car mercl("C63S Coupe");
   thread t2{ref(mercl)};
 
   t1.join();
   t2.join();
+
+  if (totaltime < mercl.get_total_time()) {
+    cout << "Sieger ist: Golf MK2 mit " << totaltime << "s" << endl;
+    cout << "Verlierer ist: " << mercl.get_car_name() << " mit " << mercl.get_total_time() << " s" << endl;
+  } else {
+    cout << "Sieger ist: " << mercl.get_car_name() << " mit " << mercl.get_total_time() << " s" << endl;
+    cout << "Verlierer ist: Golf MK2 mit " << totaltime << "s" << endl;
+  }
 
   return 0;
 }
