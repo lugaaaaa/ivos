dbccc45bdc67db4fafcd80548f6c312c50fbe34d
lugaaaaa <38163450+lugaaaaa@users.noreply.github.com>
2020-12-30 19:31:25 +0100

    Punkt 8: Verwendung der meson_options file

--- 05_sync3/src/main.cpp
+++ 05_sync3/src/main.cpp
@@ -16,16 +16,22 @@ int main(int argc, char const *argv[]) {
   Philosopher p2(2, right_m, left_m);
   Philosopher p3(3, right_m, left_m);
   Philosopher p4(4, right_m, left_m);
+  Philosopher p5(5, right_m, left_m);
+  Philosopher p6(6, right_m, left_m);
 
   thread t1(p1, sem);
   thread t2(p2, sem);
   thread t3(p3, sem);
   thread t4(p4, sem);
+  thread t5(p5, sem);
+  thread t6(p6, sem);
 
   t1.join();
   t2.join();
   t3.join();
   t4.join();
+  t5.join();
+  t6.join();
 
   return 0;
 
